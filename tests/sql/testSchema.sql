c520fc2033076f6b9749523729e463616093b11a
Ludovic Claude <ludovic.claude54@gmail.com>
2018-09-05 14:42:25 +0200

    Fix tests

--- tests/sql/testSchema.sql
+++ tests/sql/testSchema.sql
@@ -5,7 +5,7 @@ SELECT has_table( 'job_result' );
 
 SELECT has_column( 'job_result', 'job_id' );
 SELECT has_column( 'job_result', 'node' );
-SELECT col_is_pk(  'job_result', Array['job_id', 'node'] );
+SELECT col_is_pk(  'job_result', Array['job_id','shape','result_name','node'] );
 SELECT has_column( 'job_result', 'data' );
 
 SELECT * FROM finish();
